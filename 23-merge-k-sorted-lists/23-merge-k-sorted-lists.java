ff4b405124f905d421cc8e835f0863f40f69571a
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-04-23 12:25:26 +0530

    Time: 343 ms (7.04%), Space: 47.2 MB (57.72%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -12,17 +12,23 @@ class Solution {
     public ListNode mergeKLists(ListNode[] list) {
         
         ListNode start=null,head=null;
+        
+        //for inputs like [[]]
         if(list.length==0){
             return null;
         }
+
         
         if(list.length==1 && list[0]==null){
             return list[0];
         }
         
+        
         while(true){
             int min = Integer.MAX_VALUE;
             int temp = -1;
+            
+            //get the minimum of each iteration
             for(int i=0;i<list.length;i++){
                 if(list[i]!=null && min>list[i].val){
                     min = list[i].val;
@@ -30,7 +36,9 @@ class Solution {
                 }
             }
             
+            //if there is no minimum that means all the linked lists are covered
             if(temp==-1){
+                //this is to mark the next part of the last node of result null...
                 if(head!=null){
                     head.next = null;
                 }
