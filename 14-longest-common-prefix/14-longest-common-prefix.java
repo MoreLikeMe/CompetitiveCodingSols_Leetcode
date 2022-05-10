db6ebaba73feb21e9de255f9737b9c8c3b72db3b
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-05-10 23:29:16 +0530

    Time: 3 ms (33.46%), Space: 41.6 MB (70.78%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==1 || strs[0].equals("")){
+            return strs[0];
+        }
+        int itr = -1;
+        boolean flag = true;
+        while(flag){
+            itr++;
+            for(int i=0;i<strs.length-1;i++){
+                if(itr>=strs[i].length() || itr>=strs[i+1].length() || strs[i].charAt(itr)!=strs[i+1].charAt(itr)){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return strs[0].substring(0,itr);
+    }
+}
