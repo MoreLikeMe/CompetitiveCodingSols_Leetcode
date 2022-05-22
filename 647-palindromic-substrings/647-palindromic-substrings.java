c6c0bc257b7b0a88975ae2023fa9a5cdeb71e2e2
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-05-22 11:08:34 +0530

    Time: 25 ms (21.76%), Space: 50.4 MB (11.94%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -2,6 +2,10 @@ class Solution {
     public int countSubstrings(String s) {
         int len = s.length();
         int[][] dp = new int[len][len];
+        //using dynamic programming calculate the palindromes  in the string
+        //dp[i][j] is 1 if substring(i,j) is a palindrome
+        //in the next loop in line 17 we calculate the total no. of 1s that is the
+        //number of palindromes
         for(int i=len-1;i>=0;i--){
             for(int j=i;j<len;j++){
                 if(i==j || ((i+1)==j && s.charAt(i)==s.charAt(j)) || (dp[i+1][j-1]==1 && s.charAt(i)==s.charAt(j))){
