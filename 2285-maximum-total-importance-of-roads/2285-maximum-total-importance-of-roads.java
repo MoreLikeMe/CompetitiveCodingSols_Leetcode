97fb52ab04bc3f13a50ee4621c57925972a3b005
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-05-29 00:29:14 +0530

    Time: 97 ms (33.33%), Space: 126.8 MB (33.33%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -7,16 +7,16 @@ class Solution {
             freq.put(road[1], freq.getOrDefault(road[1], 0)+1);
         }
         
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        PriorityQueue<Integer> q = new PriorityQueue<>((a,b) -> b-a);
         
         for(Map.Entry<Integer, Integer> m: freq.entrySet()){
-            q.add(new int[]{m.getKey(), m.getValue()});
+            q.add(m.getValue());
         }
         
         long sum = 0;
         while(!q.isEmpty()){
-            int[] t = q.poll();
-            sum = sum + ((long)n*t[1]);
+            int t = q.poll();
+            sum = sum + ((long)n*t);
             n--;
         }
         
