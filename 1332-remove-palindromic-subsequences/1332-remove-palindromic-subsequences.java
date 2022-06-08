1588798371c3bc872b3d125ab45c7110350a9624
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-06-08 19:55:44 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (83.68%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -2,19 +2,13 @@ class Solution {
     public int removePalindromeSub(String s) {
         
         int i=0,j=s.length()-1;
-        boolean isPalin = true;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)){
-                isPalin = false;
-                break;
+                return 2;
             }
             i++; j--;
         }
         
-        if(isPalin){
-            return 1;
-        } else {
-            return 2;
-        }
+        return 1;
     }
 }
