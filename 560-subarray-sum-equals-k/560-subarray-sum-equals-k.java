a7a356d1e9dcc9d07b8865d50bd4faaf2fdb9c5a
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-06-11 21:00:45 +0530

    Time: 26 ms (76.09%), Space: 46.8 MB (69.39%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -6,13 +6,19 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             currSum += nums[i];
             
+            //if prefixSum is equal to the target
+            //then it is a subarray that is equal to k
             if(currSum==k){
                 count++;
             }
+            
+            //check in the map that (currSum-k) is present or not
+            //if present, we can say there is a subarray that is equal to k
             if(sumMap.containsKey(currSum-k)){
                 count = count + sumMap.get(currSum-k);
             }
             
+            //put the prefix sum in the map
             sumMap.put(currSum, sumMap.getOrDefault(currSum,0)+1);
         }
         
