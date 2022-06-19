373a3854d82d1e5e458dfe2b8ed24780c5e347da
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-06-19 09:53:08 +0530

    Time: 20 ms (33.33%), Space: 41.3 MB (33.33%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -1,27 +1,40 @@
 /*
-Dynamic Programming Approach
+Recursion + Memoization Approach
 */
-
 class Solution {
     public int minimumNumbers(int num, int k) {
-        
-        if(num==0){
-            return 0;
+        if(k==0){
+            k=10;
         }
         int[] dp = new int[num+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = 0;
-        for(int i=1;i<=num;i++){
-            if(i%10==k){
-                dp[i] = 1;
-                continue;
-            }
-            for(int j=k;j<=i;j=j+10){
-                if(dp[i-j]!=Integer.MAX_VALUE){
-                     dp[i] = Math.min(dp[i],dp[i-j]+1);
-                }
-            }
+        Arrays.fill(dp,-1);
+        int t = recurCall(num, k, 0, dp);
+        if(t==Integer.MAX_VALUE){
+            return -1;
         }
-        return dp[num]!=Integer.MAX_VALUE?dp[num]:-1;
+        return t;
+    }
+    
+    public int recurCall(int sum, int k, int count, int[] dp){
+        
+        if(sum==0){
+            //System.out.println("Sum:" + count);
+            return count;
+        }
+        
+        if(sum<0){
+            return Integer.MAX_VALUE;
+        }
+        
+        if(dp[sum]!=-1){
+            return dp[sum];
+        }
+        
+        int res = Integer.MAX_VALUE;
+        for(int i=k;i<=sum;i=i+10){
+            res = Math.min(res,recurCall(sum-i, k, count+1, dp));
+        }
+        dp[sum] = res;
+        return res;
     }
 }
