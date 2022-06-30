9f467bb95dc6546f430928c8a7e7b81ec0a8ef1e
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-06-30 16:13:44 +0530

    Time: 4 ms (46.94%), Space: 49.2 MB (65.94%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int jump(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+    public int jump(int[] A) {
+        int N = A.length;
+        int[] dp = new int[N];
+        Arrays.fill(dp, -1);
         dp[0] = 0;
-        for(int i=0;i<n;i++){
-            int in = 1;
-            while(in<=nums[i] && (i+in)<n){
-                dp[i+in]=Math.min(dp[i+in],dp[i]+1);
-                in++;
+        
+        int localMax = 1;
+        for(int i=0;i<N;i++){
+            if(localMax<=i){
+                return 0;
+            }
+            while(localMax<N && (i+A[i])>=localMax && dp[localMax]==-1){
+                dp[localMax] = dp[i]+1;
+                localMax++;
             }
         }
-        return dp[n-1];
+        return dp[N-1];
     }
 }
