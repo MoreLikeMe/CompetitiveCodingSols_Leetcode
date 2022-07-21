7ba4dae129d065f7e4f4236d5587779e61bcbfc8
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-07-21 13:14:45 +0530

    Time: 13 ms (44.56%), Space: 47.4 MB (27.72%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,3 +1,7 @@
+/*
+Dynamic Tabular Approach...
+
+*/
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         
@@ -19,3 +23,44 @@ class Solution {
         return dp[s.length()];
     }
 }
+
+
+/*
+Dynamic Memoization Approach
+
+
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<String>();
+        for(int i=0;i<wordDict.size();i++){
+            set.add(wordDict.get(i));
+        }
+        int[] cache = new int[s.length()];
+        Arrays.fill(cache, -1);
+        return isStringValid(s, set, 0, cache)==1?true:false;
+    }
+    
+    public int isStringValid(String s, Set<String> set, int index, int[] cache){
+        
+        if(index==s.length()){
+            return 1;
+        }
+        
+        if(cache[index]!=-1){
+            return cache[index];
+        }
+        
+        cache[index] = 0;
+        StringBuilder intermediate = new StringBuilder("");
+        for(int i=index;i<s.length();i++){
+            intermediate.append(s.charAt(i));
+            if(set.contains(intermediate.toString())){
+                cache[index] = (cache[index] | isStringValid(s, set, i+1, cache));
+            }
+        }
+        return cache[index];
+    }
+}
+
+*/
