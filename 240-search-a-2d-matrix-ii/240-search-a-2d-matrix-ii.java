ab00104d8dcbc11c4f8f72751900cb71fb503440
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-07-24 22:30:58 +0530

    Time: 14 ms (26.79%), Space: 57.6 MB (60.49%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length, col = matrix[0].length;
+        int i = 0, j = col-1;
+        while(validIndex(i,j,row,col)){
+            if(matrix[i][j]==target){
+                break;
+            }
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        return validIndex(i,j,row,col);
+    }
+    
+    public boolean validIndex(int i, int j, int row, int col){
+        if(i>=0 && j>=0 && i<row && j<col){
+            return true;
+        }
+        return false;
+    }
+}
