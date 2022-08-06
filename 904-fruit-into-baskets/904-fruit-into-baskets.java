5190721ed2795d18157d9a6a93132d98af8923fe
MoreLikeMe <46255462+MoreLikeMe@users.noreply.github.com>
2022-08-07 00:30:32 +0530

    Time: 72 ms (58.03%), Space: 97.4 MB (56.61%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -6,23 +6,21 @@ class Solution {
         int maxLen = Integer.MIN_VALUE;
         
         for(int i=0,j=0;j<fruits.length;j++){
-            
+            int key = -1;
             //when a new tree is coming on top of already 2 trees
             //remove the farthest type of tree
             //sink the array to the last occurence of the farthest tree
             if(curMap.size()==2 && !curMap.containsKey(fruits[j])){
-                //remove the farthest tree from the set
-                int farthest = -1;
-                for(int a: curMap.keySet()){
-                    if(a!=fruits[j-1]){
-                        farthest = a;
+                for(Map.Entry<Integer, Integer> m : curMap.entrySet()){
+                    if(m.getKey()!=fruits[j-1]){
+                        //set window starting position to the last 
+                        //occurence of the farthest tree + 1
+                        i = m.getValue() + 1;
+                        key = m.getKey();
                         break;
                     }
                 }
-                //set window starting position to the last 
-                //occurence of the farthest tree + 1
-                i=curMap.get(farthest)+1;
-                curMap.remove(Integer.valueOf(farthest));
+                curMap.remove(Integer.valueOf(key));
             }
             
             //add the every tree into the set
